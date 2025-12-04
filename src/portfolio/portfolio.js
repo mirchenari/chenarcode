451718f9cff53d550da154818954cba9af728d51
mirchenari <mohammadmir9691@gmail.com>
2025-12-04 12:24:18 +0330

    add portfolio

---
+++ src/portfolio/portfolio.js
@@ -0,0 +1,20 @@
+export const portfolio = [
+  {
+    name: "فروشگاه پرواز دیجیتال",
+    id: 1,
+    link: "https://parvaz-digital.liara.run/",
+    github: "https://github.com/mirchenari/parvaz-digital",
+  },
+  {
+    name: "سایت چنار کد",
+    id: 2,
+    link: "/",
+    github: "https://github.com/mirchenari/chenarcode",
+  },
+  {
+    name: "لندینگ پیج شرکتی",
+    id: 3,
+    link: "https://landing-page-ebon-sigma.vercel.app/",
+    github: "https://github.com/mirchenari/landing-page",
+  },
+];
